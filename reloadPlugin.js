584ca7115ac8582fcfe9182c21c7890158ce6877
btea <2356281422@qq.com>
2020-12-20 09:46:44 +0800

    自定义添加插件应用网址

--- reloadPlugin.js
+++ reloadPlugin.js
@@ -3,7 +3,6 @@ const path = require("path");
 const http = require("http");
 
 const name = "ReloadPlugin";
-let num = 1;
 let response;
 http.createServer((req, res) => {
     response = res;
@@ -15,7 +14,31 @@ http.createServer((req, res) => {
 }).listen(2233);
 
 class ReloadPlugin {
+    constructor(options = {}) {
+        let url = options.url || "https://github.com/";
+        this.matches = [url];
+    }
     apply(compiler) {
+        compiler.hooks.afterPlugins.tap(name, (compiler) => {
+            let _path = path.resolve(__dirname, "./public/manifest.json");
+            fs.readFile(_path, (err, chunk) => {
+                if (err) {
+                    return;
+                }
+                chunk = chunk.toString();
+                chunk = JSON.parse(chunk);
+                chunk["content_scripts"][0].matches = this.matches;
+                chunk = JSON.stringify(chunk, null, " ".repeat(4));
+                fs.writeFile(_path, chunk, "utf-8", (err) => {
+                    if (err) {
+                        console.log("matches url 修改失败");
+                        return;
+                    }
+                    console.log("matches url 修改成功");
+                });
+            });
+        });
+
         compiler.hooks.done.tap(name, (stats) => {
             console.log("\r\n   开始更新插件   \r\n");
             try {
