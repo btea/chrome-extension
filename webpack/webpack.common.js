c376fd9b1064b93d667fa6defe180ac6d4b74f82
btea <2356281422@qq.com>
2020-12-20 09:54:43 +0800

    webpack插件参数说明注释

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -156,7 +156,14 @@ module.exports = {
                 // { from: 'extens/popup.html', to: 'extension' }
             ],
         }),
-        new ReloadPlugin(),
+        /**
+         * params Object
+         * url(默认值为 https://github.com/) 指定插件生效的网址
+         * 注：网址后面应带上 '/' 否则插件会报错  Invalid value for 'content_scripts[0].matches[0]': Empty path.
+         */
+        new ReloadPlugin({
+            url: "https://www.bilibili.com/",
+        }),
         // new webpack.NamedModulesPlugin(),
         // new webpack.HotModuleReplacementPlugin(),
         new VueLoaderPlugin(),
