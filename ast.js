bc79842b54993bd8dc2036a5576c38b838bdf9b5
btea <2356281422@qq.com>
2021-04-20 18:08:04 +0800

    @babel/generator

--- ast.js
+++ ast.js
@@ -1,9 +1,10 @@
-const fs = require("fs");
-const path = require("path");
-const os = require("os");
-const parser = require("@babel/parser");
-const traverse = require("@babel/traverse").default;
-const core = require("@babel/core");
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+const parser = require('@babel/parser');
+const traverse = require('@babel/traverse').default;
+const core = require('@babel/core');
+const generate = require('@babel/generator').default;
 
 let port = 2233;
 function getIp() {
@@ -11,30 +12,30 @@ function getIp() {
     try {
         WLAN = os.networkInterfaces().WLAN;
     } catch (error) {
-        console.log("获取ip失败");
+        console.log('获取ip失败');
         throw Error(error);
     }
     if (WLAN) {
         for (let i = 0; i < WLAN.length; i++) {
-            if (WLAN[i].family === "IPv4") {
-                return WLAN[i].address || "";
+            if (WLAN[i].family === 'IPv4') {
+                return WLAN[i].address || '';
             }
         }
     }
 }
 
-const _path = path.resolve(__dirname, "./src/background.js");
+const _path = path.resolve(__dirname, './src/background.js');
 function editCode() {
     fs.readFile(_path, (err, chunk) => {
         chunk = chunk.toString();
         const ast = parser.parse(chunk, {
-            sourceType: "module",
+            sourceType: 'module'
         });
         let num = 1;
-        ast.program.body.forEach((item) => {
-            if ("FunctionDeclaration" === item.type) {
+        ast.program.body.forEach(item => {
+            if ('FunctionDeclaration' === item.type) {
                 let id = item.id;
-                if (id.name === "link") {
+                if (id.name === 'link') {
                     let body = item.body.body[0];
                     let _id = body.declarations[0];
                     let v = `"http://${getIp()}:${port}"`;
@@ -45,14 +46,35 @@ function editCode() {
             }
         });
         let transSource = core.transformFromAstSync(ast, null, {
-            configFile: false,
+            configFile: false
         }).code;
-        fs.writeFile(_path, transSource, (err) => {
+        const output = generate(
+            ast,
+            {
+                auxiliaryCommentBefore: '这是放在开头的注释',
+                auxiliaryCommentAfter: '这是放在结尾的注释',
+                compact: false
+            },
+            transSource
+        );
+        const code = output.code;
+        let codes = code.split(/\n/g);
+        codes = codes.map(c => {
+            let reg = /^ +/;
+            if (reg.test(c)) {
+                const len = c.match(reg)[0].length;
+                c = ' '.repeat(len) + c;
+                return c;
+            }
+            return c;
+        });
+        const result = codes.join('\n');
+        fs.writeFile(_path, result, err => {
             if (err) {
-                console.log("写入失败");
+                console.log('写入失败');
                 return;
             }
-            console.log("写入成功");
+            console.log('写入成功');
         });
         // traverse(ast, {
         //     FunctionDeclaration: function (path) {
@@ -62,5 +84,5 @@ function editCode() {
         // });
     });
 }
-
+editCode();
 module.exports = editCode;
