1e8eee234135569410d405b5e1d91425a043952d
btea <2356281422@qq.com>
2021-08-25 11:07:11 +0800

    feat: setup

--- src/Popup.vue
+++ src/Popup.vue
@@ -1,37 +1,34 @@
 <template>
-    <div class="box">
-        <div class="item" v-for="menu in list" :key="menu.val">{{ menu.name }}</div>
-    </div>
+	<div class="box">
+		<div class="item" v-for="menu in list" :key="menu.val">
+			{{ menu.name }}
+		</div>
+	</div>
 </template>
-<script>
+<script setup>
 import { reactive } from "vue";
-export default {
-    setup() {
-        let list = reactive([
-            { name: "图片", val: "img" },
-            { name: "文字", val: "text" },
-            { name: "图片", val: "img" },
-            { name: "文字", val: "text" },
-            { name: "内容", val: "content" },
-        ]);
-        return { list };
-    },
-};
+let list = reactive([
+	{ name: "图片", val: "img" },
+	{ name: "文字", val: "text" },
+	{ name: "图片", val: "img" },
+	{ name: "文字", val: "text" },
+	{ name: "内容", val: "content" }
+]);
 </script>
 <style>
 .box {
-    width: 450px;
-    display: flex;
-    flex-wrap: wrap;
+	width: 450px;
+	display: flex;
+	flex-wrap: wrap;
 }
 .box .item {
-    width: 150px;
-    height: 50px;
-    cursor: pointer;
-    font-size: 20px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: #6cf;
+	width: 150px;
+	height: 50px;
+	cursor: pointer;
+	font-size: 20px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	color: #6cf;
 }
 </style>
